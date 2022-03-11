07d0cd67d54e4eac245588a60093b77401db330e
ellikamishra <ellikamishra009@gmail.com>
2022-03-11 16:18:00 +0530

    file saving

--- MITRE-frontend/src/Components/PolicyScreen/PolicyScreen.jsx
+++ MITRE-frontend/src/Components/PolicyScreen/PolicyScreen.jsx
@@ -5,37 +5,51 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/fontawesome-free-solid';
 import { Link } from 'react-router';
 import './policyscreen.css';
+import axios from 'axios';
 // import { Button } from 'bootstrap';
-import upload from '../../images/upload.png'
 
 const PolicyScreen=props=>{
 
-    const hiddenFileInput=React.useRef(null);
-    const handleClick=event=>{
-        hiddenFileInput.current.click();
-    }
+    // const hiddenFileInput=React.useRef(null);
+    // const handleClick=event=>{
+    //     hiddenFileInput.current.click();
+    // }
+
+    const [file, setFile] = useState();
+
     const handleChange=event=>{
         const fileUpload=event.target.files[0];
-        props.handleFile(fileUpload);
+        //props.handleFile(fileUpload);
+        setFile(fileUpload);
+        //uploadFile(file);
     }
+
+    function uploadFile(e) {
+        e.preventDefault();
+        const fileUpload=e.target.files[0];
+        const formData = new FormData();
+        formData.append("file", fileUpload);
+        console.log(formData)
+        axios
+          .post("http://127.0.0.1:5000/upload", formData)
+          .then(res => console.log(res))
+          .catch(err => console.warn(err));
+    }
+
+
     return(
-          
-        <div className = "policy">
-            <div className="policy_title">
-                <h1> Upload a Report</h1>
-                <img src={upload} alt=""/>
+        <div className="outer">
+             <div className = "search-section">
+             <input className = "input-section" type="file" name="file" onChange={uploadFile}/>
+                {/* <input   type="file" className = "input-section" ref={hiddenFileInput} onChange={handleChange}/> */}
+                {/* <FontAwesomeIcon  id="i" className="fas" icon={faSearch} />      */}
+            </div>
+
+            <div className = "search-results"> 
+                
             </div>
             
-
-            <div className="policy_searchbar">
-                Choose a File :
-                <input   type="file" className = "input-section" ref={hiddenFileInput} onChange={handleChange}/>
-
-            </div>
-
-            <h1 className = "policy_result">Search Results</h1>
-            {/* <FontAwesomeIcon  id="i" className="fas" icon={faSearch} />      */}
-        </div> 
+        </div>
     );
 
 
